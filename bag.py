e31609dccc679a570235f7583f393c2fb96103d8
Sander Mack-Crane <smackcrane@gmail.com>
2021-06-18 15:19:31 -0700

    bug fix: make paths absolute

--- bag.py
+++ bag.py
@@ -7,16 +7,18 @@ import pandas as pd
 import json
 import datetime
 
+script_path = os.path.dirname(os.path.realpath(__file__))
+
 arg_iter = iter(sys.argv[1:])
 spec = None
 for arg in arg_iter:
-    if spec is None and os.path.exists(f'surveys/{arg}.yaml'):
-        data_path = f'data/{arg}.csv'
+    if spec is None and os.path.exists(f'{script_path}/surveys/{arg}.yaml'):
+        data_path = f'{script_path}/data/{arg}.csv'
         try:
             data = pd.read_csv(data_path)
         except FileNotFoundError:
             data = None
-        with open(f'surveys/{arg}.yaml', 'r') as f:
+        with open(f'{script_path}/surveys/{arg}.yaml', 'r') as f:
             spec = yaml.load(f, Loader=yaml.SafeLoader)
     else:
         raise Exception(f'Argument {arg} not supported or survey not found')
