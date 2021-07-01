c2f630b34603ce056cc092534d68cf2e5eb07f01
Sander Mack-Crane <smackcrane@gmail.com>
2021-07-01 11:20:16 -0700

    option to visualize values in calendar, and simple visualization function

--- DataCalendar.py
+++ DataCalendar.py
@@ -10,7 +10,7 @@ class DataCalendar(calendar.TextCalendar):
 
     # takes dict with ISO dates as keys and corresponding data as values
     # returns string containing text calendar with dates filled with data
-    def formatdata(self, data_dict):
+    def formatdata(self, data_dict, visualize=lambda x: x):
         # start output string
         out = ''
 
@@ -21,7 +21,8 @@ class DataCalendar(calendar.TextCalendar):
             terminal_width = 76
 
         # compute date box width
-        max_len = max(map(lambda x: len(str(x)), data_dict.values()))
+        val_length = lambda x: len(str(visualize(x)))
+        max_len = max(map(val_length, data_dict.values()))
         date_width = min((terminal_width-6) // 7, max_len)
         # compute total output width
         out_width = date_width*7 + 6
@@ -41,7 +42,8 @@ class DataCalendar(calendar.TextCalendar):
         while counter <= last:
             # format one month of data
             out += self.formatmonthdata(
-                    data_dict, counter.year, counter.month, date_width
+                    data_dict, counter.year, counter.month, date_width,
+                    visualize
                     )
             # increment counter
             if counter.month == 12:
@@ -59,7 +61,10 @@ class DataCalendar(calendar.TextCalendar):
     # takes dict with ISO dates as keys and corresponding data as values
     # as well as year and month and width of date box
     # prints text calendar of single month with dates filled with data
-    def formatmonthdata(self, data_dict, year, month, date_width=2):
+    def formatmonthdata(
+            self, data_dict, year, month, date_width=2,
+            visualize = lambda x: x
+            ):
         # start output string with month and weekdays header
         out = self.formatmonthname(year, month, date_width*7 + 6)
         out += '\n'*1
@@ -72,6 +77,8 @@ class DataCalendar(calendar.TextCalendar):
             for (day, day_of_wk) in week:
                 ISO_date = datetime.date(year, month, day).isoformat()
                 value = data_dict.get(ISO_date, '')
+                # extract relevant information for visualization
+                value = visualize(value)
                 s = str(value).center(date_width)
                 if len(s) > date_width:
                     s = s[:date_width]
@@ -80,4 +87,9 @@ class DataCalendar(calendar.TextCalendar):
             out += '\n'*1
 
         return out
+
+    # visualization function
+    # returns a solid block if input is true, else 'empty block' i.e. spaces
+    def truth_blocks(self, value):
+        return '\u2588'*2 if value else ' '*2
 
